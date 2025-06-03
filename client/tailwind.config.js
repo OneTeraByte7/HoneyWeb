42fb72500fc87efee977c2b39516ffeb20437133
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-06-03 13:40:23 +0530

    Setup Done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
